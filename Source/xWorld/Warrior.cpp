0a4edf6e30e04211c0599c00b713ca47de2875c8
CsloudX <35988767+CsloudX@users.noreply.github.com>
2019-06-05 08:45:54 +0800

    12
    11

--- Source/xWorld/Warrior.cpp
+++ Source/xWorld/Warrior.cpp
@@ -10,7 +10,7 @@ AWarrior::AWarrior()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+	UPhysicsThrusterComponent
 }
 
 // Called when the game starts or when spawned
